81f4ffa2579794ffb7df45a906dbfb151a5f5899
Wade <115143365+Waddenn@users.noreply.github.com>
2023-10-20 15:02:33 +0200

    updating manifest

--- src/js/background.js
+++ src/js/background.js
@@ -1,11 +1,14 @@
 const browserAction = chrome || browser;
 
 browserAction.browserAction.onClicked.addListener(() => {
-  browserAction.tabs.executeScript({ file: "/js/overlay_remover.js" }, () => {
-    if (chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-      return;
+  browserAction.tabs.executeScript(
+    { file: "src/js/overlay_remover.js" },
+    () => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+      browserAction.tabs.executeScript({ code: "overlayRemoverRun();" });
     }
-    browserAction.tabs.executeScript({ code: "overlayRemoverRun();" });
-  });
+  );
 });
